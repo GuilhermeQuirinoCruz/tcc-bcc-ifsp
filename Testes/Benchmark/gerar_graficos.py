fe4106f38135adb443fa42671129c4e2b8c25285
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2024-10-09 07:40:08 -0300

    Início dos gráficos

---
+++ Testes/Benchmark/gerar_graficos.py
@@ -0,0 +1,20 @@
+import pandas
+import numpy
+import matplotlib.pyplot as plot
+
+CAMINHO_CSV = 'csv/'
+
+
+def gerar_graficos_carregamento():
+    print('Gerando...')
+
+
+def gerar_graficos_transacao():
+    print('Gerando...')
+
+
+def main():
+    print('Gerando gráficos...')
+
+if __name__ == '__main__':
+    main()
